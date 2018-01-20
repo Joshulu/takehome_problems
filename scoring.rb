2dfce1543a98db9d006fd2b97c7c256bf5c86d75
mstruve <mollylbs@gmail.com>
2018-01-20 14:15:34 -0600

    score spanish or english words, upper and lowercase with custom scoring option for new languages

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,55 @@
 # frozen_string_literal: true
 
 class Scoring
-  def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
+
+  attr_accessor :language, :scoring_hash
+
+  def self.english_scoring_hash
+    hash = {}
+    %w[A E I O U L N R S T].each{|l| hash[l.to_sym] = 1 }
+    %w[D G].each{|l| hash[l.to_sym] = 2 }
+    %w[B C M P].each{|l| hash[l.to_sym] = 3 }
+    %w[F H V W Y].each{|l| hash[l.to_sym] = 4 }
+    %w[K].each{|l| hash[l.to_sym] = 5 }
+    %w[J X].each{|l| hash[l.to_sym] = 8 }
+    %w[Q Z].each{|l| hash[l.to_sym] = 10 }
+    hash
+  end
+
+  def self.spanish_scoring_hash
+    hash = {}
+    %w[A E O S I U N L R T].each{|l| hash[l.to_sym] = 1 }
+    %w[C D G].each{|l| hash[l.to_sym] = 2 }
+    %w[B M P].each{|l| hash[l.to_sym] = 3 }
+    %w[F H V Y].each{|l| hash[l.to_sym] = 4 }
+    %w[J].each{|l| hash[l.to_sym] = 6 }
+    %w[K LL Ñ Q RR W X].each{|l| hash[l.to_sym] = 8 }
+    %w[Z].each{|l| hash[l.to_sym] = 10 }
+    hash
+  end
+
+  SCORING_HASHES = {
+    :english => self.english_scoring_hash,
+    :spanish => self.spanish_scoring_hash,
+  }.freeze
+
+  def initialize(language, custom_scoring_hash = {})
+    self.language = language
+    if !custom_scoring_hash.empty?
+      self.scoring_hash = custom_scoring_hash
     else
-      0
+      self.scoring_hash = SCORING_HASHES[language]
     end
   end
+
+  def score_letter(letter)
+    scoring_hash.fetch(letter.upcase.to_sym, 0)
+  end
+
+  # def score_word(word)
+  #   word.gsub!(/LL|RR/, '*')
+  #   word.each_char.sum do |letter|
+  #     score_letter(letter)
+  #   end
+  # end
 end
--- scoring_spec.rb
+++ scoring_spec.rb
@@ -3,10 +3,10 @@
 require_relative 'scoring'
 
 RSpec.describe Scoring do
-  subject { described_class.new }
-
   describe '#score_letter' do
     context 'in English' do
+      subject { described_class.new(:english) }
+
       %w[A E I O U L N R S T].each do |letter|
         it "scores a #{letter} as 1 point" do
           expect(subject.score_letter(letter))
@@ -55,10 +55,140 @@ RSpec.describe Scoring do
             .to eq 10
         end
       end
+
+      context 'letters are lowercase' do
+        %w[a e i o u l n r s t].each do |letter|
+          it "scores a #{letter} as 1 point" do
+            expect(subject.score_letter(letter))
+              .to eq 1
+          end
+        end
+
+        %w[d g].each do |letter|
+          it "scores a #{letter} as 2 point" do
+            expect(subject.score_letter(letter))
+              .to eq 2
+          end
+        end
+
+        %w[b c m p].each do |letter|
+          it "scores a #{letter} as 3 point" do
+            expect(subject.score_letter(letter))
+              .to eq 3
+          end
+        end
+
+        %w[f h v w y].each do |letter|
+          it "scores a #{letter} as 4 point" do
+            expect(subject.score_letter(letter))
+              .to eq 4
+          end
+        end
+
+        %w[k].each do |letter|
+          it "scores a #{letter} as 5 point" do
+            expect(subject.score_letter(letter))
+              .to eq 5
+          end
+        end
+
+        %w[j x].each do |letter|
+          it "scores a #{letter} as 8 point" do
+            expect(subject.score_letter(letter))
+              .to eq 8
+          end
+        end
+
+        %w[q z].each do |letter|
+          it "scores a #{letter} as 10 point" do
+            expect(subject.score_letter(letter))
+              .to eq 10
+          end
+        end
+      end
     end
 
     context 'in Spanish' do
-      pending 'deliverable'
+      subject { described_class.new(:spanish) }
+
+      %w[A E O S I U N L R T].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(subject.score_letter(letter))
+            .to eq 1
+        end
+      end
+
+      %w[C D G].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          expect(subject.score_letter(letter))
+            .to eq 2
+        end
+      end
+
+      %w[B M P].each do |letter|
+        it "scores a #{letter} as 3 point" do
+          expect(subject.score_letter(letter))
+            .to eq 3
+        end
+      end
+
+      %w[F H V Y].each do |letter|
+        it "scores a #{letter} as 4 point" do
+          expect(subject.score_letter(letter))
+            .to eq 4
+        end
+      end
+
+      %w[J].each do |letter|
+        it "scores a #{letter} as 5 point" do
+          expect(subject.score_letter(letter))
+            .to eq 6
+        end
+      end
+
+      %w[K LL Ñ Q RR W X].each do |letter|
+        it "scores a #{letter} as 8 point" do
+          expect(subject.score_letter(letter))
+            .to eq 8
+        end
+      end
+
+      %w[Z].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          expect(subject.score_letter(letter))
+            .to eq 10
+        end
+      end
+    end
+
+    context 'custom scoring' do
+      subject { described_class.new(:klingon, {:A => 100, :B => 50}) }
+
+      %w[A].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(subject.score_letter(letter))
+            .to eq 100
+        end
+      end
+
+      %w[B].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          expect(subject.score_letter(letter))
+            .to eq 50
+        end
+      end
     end
   end
+
+  # describe '#score_word' do
+  #   it 'scores a word in english' do
+  #     subject = Scoring.new(:english)
+  #     expect(subject.score_word('STRRUVE')).to eq(10)
+  #   end
+
+  #   it 'scores a word in spanish' do
+  #     subject = Scoring.new(:spanish)
+  #     expect(subject.score_word('STRRUVE')).to eq(16)
+  #   end
+  # end
 end
