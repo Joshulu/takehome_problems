ed77812bf5fefb2162e382f698adfa6af79c9b09
Jared Davis <jared@haiq.us>
2018-01-22 11:30:58 -0600

    code at my arbitrary "stopping point"
    I deemed consideration of dependency injection as a basic indicator that a candidate would be above the "super junior" level of skill, and so stopped the dry run when my last refactor would set up DI.

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,41 @@
 # frozen_string_literal: true
 
 class Scoring
+  attr_reader :dictionary
+
+  DICTIONARIES = {
+    :english => { 1  => %w[A E I O U L N R S T],
+                  2  => %w[D G],
+                  3  => %w[B C M P],
+                  4  => %w[F H V W Y],
+                  5  => %w[K],
+                  8  => %w[J X],
+                  10 => %w[Q Z] },
+    :spanish => { 1  => %w[A E I O U L N R S T],
+                  2  => %w[C D G],
+                  3  => %w[B M P],
+                  4  => %w[F H V Y],
+                  6  => %w[J],
+                  8  => %w[K LL Ñ Q RR W X],
+                  10 => %w[Z] }
+  }
+
+  def initialize(language = :english)
+    @dictionary = configure(language)
+  end
+
   def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
-    else
-      0
-    end
+    dictionary.fetch(letter, 0)
+  end
+  
+  private
+
+  def configure(language)
+    DICTIONARIES.fetch(language)
+                .each_with_object({}) do |(score, letters), dict|
+                  letters.each do |letter|
+                    dict[letter] = score
+                  end
+                end
   end
 end
